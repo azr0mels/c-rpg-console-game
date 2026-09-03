dd1699dbbd59fa0aec48181dd84e795cc1742c5e
Azra Melis Şen <azramelis001@gmail.com>
2026-09-04 02:24:41 +0300

    Improve game menu functionality and syntax
    Refactored game menu logic and fixed syntax errors.

--- game.c
+++ game.c
@@ -1,35 +1,41 @@
 #include <stdio.h>
-int main() {
-    int i=0;
+int main()
+{
+    int i = 0;
+    int health = 100;
+    int mana = 50;
+    
     printf("\n-------MENU-------\n");
-    printf("Choose one:\n");
-    scanf("%d",&i);
-    printf("1-Use health potion(+25 health)\n");
-    printf("2- Cast spell(-15 mana)\n");
-    printf("3-Show status\n");
+    printf("1- Use health potion (+25 health)\n");
+    printf("2- Cast spell (-15 mana)\n");
+    printf("3- Show status\n");
     printf("4- Exit\n");
-    int health, mana;
-    health=100;
-    mana=50;
-    if (i<=0)
-        while (i<=0) {
-          i++;
-        }
-    }
-    else if (i=1){
-        health-= 25;}
-    else if (i=2) {
-        if (mana>=15) {
-            mana-=15;
-        }
+    printf("Choose one: ");
+
+    scanf("%d", &i);
+    if (i == 1) {
+        health += 25; 
+        printf("Potion used! Health: %d\n", health)
+                }
+    else if (i == 2) {
+        if (mana >= 15) {
+            mana -= 15;
+            printf("Spell casted! Mana: %d\n", mana);
+                        }
         else {
-            printf("insufficient mana.");
-        }
-    }
-    else if (i=3) {
-        printf("health:%d\n", health);
-        printf("mana;%d\n", mana);
-    }
-    else(i=4);
+            printf("Insufficient mana.\n");
+             }
+                     }
+    else if (i == 3) {
+        printf("Health: %d\n", health);
+        printf("Mana: %d\n", mana);
+                     }
+    else if (i == 4) {
+        printf("Exiting...\n");
+                     }
+    else {
+        printf("Invalid choice. Try again.\n");
+         }
+
     return 0;
 }
